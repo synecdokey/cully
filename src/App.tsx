896f2bca8ee4d094f8d15787004acc0a7d73dd26
Emilia Zapata <zapata.contact@gmail.com>
2022-03-04 19:04:52 +1300

    feat(App): add base layout with image
    Made some assumptions on my end since I can't have a discussion with the
    designer, but the gist was to not let the image get bigger than the
    actual viewport

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 const App = () => {
   return (
-    <header className="text-black">
-      <h1>Cully</h1>
-    </header>
+    <main className="w-full bg-off-white h-screen">
+      <img
+        src="/img.jpg"
+        alt=""
+        className="max-w-[85%] max-h-[100vh] mx-auto py-5"
+      />
+    </main>
   );
 };
 
