4b5f8fa54772b85a7686a58b9c0de166a86e7fea
Emilia Zapata <zapata.contact@gmail.com>
2022-03-04 18:26:13 +1300

    feat(tailwind): add colours from figma file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "off-white": "#E5E5E5",
+        "brand-ui-6": "#f2f2f2",
+        "black-ui": "#1c1c1c",
+        "grey-ui": "#828282",
+        "light-grey-ui": "#e2e2e3",
+        "red-ui": "#eb5757",
+      },
+    },
   },
   plugins: [],
 };
